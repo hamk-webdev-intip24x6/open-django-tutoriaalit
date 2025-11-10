c45bd05ce0e2b0798b71991870b5d0b04ad68642
petrikuittinen <petri.kuittinen@hamk.fi>
2025-11-10 13:34:14 +0200

    models.py ja admin.py lisätty

--- myproject/hello/admin.py
+++ myproject/hello/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_text', 'date')
+    list_filter = ['date']
+    search_fields = ['message_text']
+
+admin.site.register(Message, MessageAdmin)
--- myproject/hello/models.py
+++ myproject/hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    message_text = models.CharField(max_length=250)
+    date = models.DateTimeField('date created', auto_now_add=True)
+    def __str__(self):
+        return self.message_text
