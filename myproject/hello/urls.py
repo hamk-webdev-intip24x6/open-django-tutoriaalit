f7f1f06cb25581c10b49fc3ba4aa90358e9441d3
petrikuittinen <petri.kuittinen@hamk.fi>
2025-11-10 13:45:02 +0200

    django hello tutorial completed

--- myproject/hello/urls.py
+++ myproject/hello/urls.py
@@ -3,5 +3,7 @@ from . import views
  
 app_name = 'hello'
 urlpatterns = [
-    path('', views.index, name='index'), # "hello:index"
+    #path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('second/', views.second, name='second'),
 ]
--- myproject/hello/views.py
+++ myproject/hello/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Message
  
-def index(request):
+# def index(request):
+#     context = {"latest_message_list": Message.objects.order_by('-date')[:10]}
+#     return render(request, 'hello/index.html', context)
+
+def second(request):
     context = {}
-    return render(request, 'hello/index.html', context)
+    return render(request, 'hello/second.html', context)
+
+class IndexView(ListView):
+    template_name = 'hello/index.html'
+    context_object_name = 'latest_message_list'
+
+    def get_queryset(self):
+        """Return the last 10 published messages."""
+        return Message.objects.order_by('-date')[:10]
